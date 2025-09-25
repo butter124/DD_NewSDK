53f9e61fec8da99cd6d7a24a5c83bf5077f207b2
butter124 <stolenk420@gmail.com>
2025-09-25 00:31:27 -0700

    improved comment

--- DD_NewSDK/src/menu_main.cpp
+++ DD_NewSDK/src/menu_main.cpp
@@ -2104,7 +2104,7 @@ void MenuMain::Lua() {
     config->L->execute_lua_file(path);
   }
 
-  if (ImGui::Button("copy to clipboard", ImVec2(0, 0))) {
+  if (ImGui::Button("copy pos to clipboard", ImVec2(0, 0))) {
     char buf[128];
     snprintf(buf, sizeof(buf), "%.2f, %.2f, %.2f",
              config->GetPlayerPawn()->Location.X,
